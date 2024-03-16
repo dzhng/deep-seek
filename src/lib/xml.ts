c3772a369133c7da810a506dfe2712157b1a4189
David Zhang <david@Davids-Laptop.local>
2024-03-16 14:29:45 -0700

    blah

--- src/lib/xml.ts
+++ src/lib/xml.ts
@@ -2,5 +2,6 @@ import { toXML as toXMLImpl, XmlElement, XmlOptions } from 'jstoxml';
 
 // wrap the toXML method to put in some default options
 export function toXML(obj?: XmlElement | XmlElement[], options?: XmlOptions) {
-  return toXMLImpl(obj, { ...options, depth: 100, indent: '' });
+  // adding trim since `toXML` tends to add new lines before
+  return toXMLImpl(obj, { ...options, depth: 100, indent: '' }).trim();
 }
