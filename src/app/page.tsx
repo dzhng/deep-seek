799b2870cfc8a40600ada3176e01c8958eb9667e
David Zhang <dzz0615@gmail.com>
2024-03-15 22:00:59 -0700

    added form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,68 @@
 'use client';
 
-import { useCallback, useState } from 'react';
+import { useCallback } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 import toast from 'react-hot-toast';
+import { z } from 'zod';
 
 import { trpc } from '@/lib/trpc/client';
 import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 
-export default function Home() {
-  const [input, setInput] = useState('');
-  const runQuery = trpc.run.useMutation();
+const formSchema = z.object({
+  prompt: z.string().min(2, {
+    message: 'Prompt must be at least 2 characters.',
+  }),
+});
 
-  const onSubmit = useCallback(() => {
-    runQuery.mutate({ prompt: input });
-    setInput('');
-    toast('Submitted');
-  }, [input, runQuery]);
+export default function Home() {
+  const runQuery = trpc.run.useMutation();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: '',
+    },
+  });
+
+  const onSubmit = useCallback(
+    (values: z.infer<typeof formSchema>) => {
+      runQuery.mutate({ prompt: values.prompt });
+      form.reset();
+      toast('Submitted');
+    },
+    [form, runQuery],
+  );
 
   return (
     <main className="min-h-screen space-y-3 p-24">
-      <h1 className="text-lg font-bold">Prompt</h1>
-      <Input value={input} onChange={e => setInput(e.target.value)} />
-      <Button onClick={onSubmit}>Submit</Button>
+      <h1 className="text-lg font-bold">Deep Seek</h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+          <FormField
+            control={form.control}
+            name="prompt"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Query</FormLabel>
+                <FormControl>
+                  <Input placeholder="Sam Altman" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
     </main>
   );
 }
