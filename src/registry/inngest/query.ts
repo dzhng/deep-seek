7592eb603054ea0b45d7066f0d41f8ae3530afe2
David Zhang <dzz0615@gmail.com>
2024-03-22 17:51:57 -0700

    blah

--- src/registry/inngest/query.ts
+++ src/registry/inngest/query.ts
@@ -11,8 +11,6 @@ export const run = inngest.createFunction(
   { id: 'run-query', name: 'Run query' },
   { event: 'app/query.run' },
   async ({ event, step }) => {
-    console.info('Running query', event.data.prompt);
-
     const preprocessed = await step.run('preprocess-prompt', async () =>
       preprocessPrompt({ userPrompt: event.data.prompt }),
     );
@@ -57,6 +55,7 @@ export const run = inngest.createFunction(
             content: [retrieveRes[row]],
           });
           table[row][column + 1] = cell;
+          return cell;
         });
 
         promises.push(promise);
