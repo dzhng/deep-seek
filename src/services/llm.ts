7d7543f321114a56753905860e6b3573e0a03117
David Zhang <dzz0615@gmail.com>
2024-03-27 23:15:15 -0700

    removed unused llms

--- src/services/llm.ts
+++ src/services/llm.ts
@@ -1,9 +1,4 @@
-import {
-  AnthropicChatApi,
-  ChatRequestMessage,
-  GroqChatApi,
-  OpenAIChatApi,
-} from 'llm-api';
+import { AnthropicChatApi, ChatRequestMessage } from 'llm-api';
 import { partial } from 'lodash';
 import { z } from 'zod';
 import { chat, completion, RequestOptions, Response } from 'zod-gpt';
@@ -17,29 +12,6 @@ type Chat = <T extends z.ZodType = z.ZodString>(
   messages: ChatRequestMessage[],
   opt?: Partial<RequestOptions<T>>,
 ) => Promise<Response<T>>;
-
-export const gpt3xl = new OpenAIChatApi(
-  {
-    apiKey: process.env.AZURE_OPENAI_KEY!,
-    azureEndpoint: process.env.AZURE_OPENAI_ENDPOINT,
-    azureDeployment: 'gpt-35-16k',
-  },
-  { temperature: 0, contextSize: 16_000 },
-);
-export const gpt3xlCompletion: Completion = partial(completion, gpt3xl);
-export const gpt3xlChat: Chat = partial(chat, gpt3xl);
-
-export const gpt4turbo = new OpenAIChatApi(
-  {
-    apiKey: process.env.AZURE_OPENAI_KEY!,
-    azureEndpoint: process.env.AZURE_OPENAI_ENDPOINT,
-    azureDeployment: 'gpt-4-128k',
-  },
-  // context size is technically 127793 tokens, but leave some room for margins
-  { temperature: 0, contextSize: 120_000, stream: true },
-);
-export const gpt4TurboCompletion: Completion = partial(completion, gpt4turbo);
-export const gpt4TurboChat: Chat = partial(chat, gpt4turbo);
 
 export const sonnet = new AnthropicChatApi(
   { apiKey: process.env.ANTHROPIC_KEY },
@@ -76,15 +48,3 @@ export const opus = new AnthropicChatApi(
 );
 export const opusCompletion: Completion = partial(completion, opus);
 export const opusChat: Chat = partial(chat, opus);
-
-export const mixtral = new GroqChatApi(
-  { apiKey: process.env.GROQ_KEY! },
-  {
-    model: 'mixtral-8x7b-32768',
-    stream: false,
-    temperature: 0,
-    contextSize: 32_000,
-  },
-);
-export const mixtralCompletion: Completion = partial(completion, mixtral);
-export const mixtralChat: Chat = partial(chat, mixtral);
